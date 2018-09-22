6b66cfaca59e4493c69ebae985dfc38c7896f2b0
Ian Busko <ian.busko@gmail.com>
2018-09-22 15:41:54 -0700

    Fix Twitter Sharing text

--- src/shareLinkUrls.js
+++ src/shareLinkUrls.js
@@ -1,13 +1,13 @@
 import { ShareLinkType } from './shareLinkTypes';
 
-function getShareLinkUrl(type, url) {
+function getShareLinkUrl(type, url, sharingText) {
   switch (type) {
     case ShareLinkType.Facebook:
       return encodeURI(`https://www.facebook.com/sharer/sharer.php?u=${url}`);
     case ShareLinkType.Email:
       return encodeURI(`mailto:?subject=${'The Noted Project'}&body=${url}`);
     case ShareLinkType.Twitter:
-      return encodeURI(`https://twitter.com/share?url=${url}`);
+      return encodeURI(`https://twitter.com/share?url=https://${encodeURI(url)}&text=${sharingText}`);
     default:
       return url;
   }
