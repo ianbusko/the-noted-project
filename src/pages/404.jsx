8994b0c7ba38d3cec304f12d1f4778c671f6c20d
Ian Busko <ian.busko@gmail.com>
2018-09-23 20:47:38 -0700

    Export layout with meta data from layout component

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import Layout from '../components/layout';
-import withMetaData from '../layouts/withMetadata';
-
-const LayoutWithMetaData = withMetaData(Layout);
 
 const NotFoundPage = () => (
-  <LayoutWithMetaData isPlain>
+  <Layout isPlain>
     <section>
       {/* eslint-disable-next-line */}
       Whoops! It looks like the page you searched for doesn't exist.
     </section>
-  </LayoutWithMetaData>
+  </Layout>
 );
 
 export default NotFoundPage;
