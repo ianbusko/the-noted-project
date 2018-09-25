b57051218951aabb9a86a7b181af7a68fb3b3f38
Ian Busko <ian.busko@gmail.com>
2018-09-24 20:42:34 -0700

    Prevent excessive rendering on story slides

--- src/components/story/navDots.jsx
+++ src/components/story/navDots.jsx
@@ -2,28 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../../less/navDots.less';
 
-const NavDots = ({ targets, activeDot, onDotClicked }) => (
-  <div className="nav-dots">
-    <ul>
-      {targets.map((target, index) => (
-        <li
-          className={`${activeDot === index ? 'current' : ''}`}
-          key={target.id}
-        >
-          {/* eslint-disable-next-line */}
-          <a
-            role="button"
-            onClick={() => onDotClicked(index)}
-          >
-            <span style={{ display: 'none' }}>
-              {`Scroll to slide ${index}`}
-            </span>
-          </a>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+class NavDots extends React.PureComponent {
+  componentWillUpdate(newval, oldval) {
+    console.log('update', newval, oldval);
+  }
+
+  render() {
+    const { targets, activeDot, onDotClicked } = this.props;
+    return (
+      <div className="nav-dots">
+        <ul>
+          {targets.map((target, index) => (
+            <li
+              className={`${activeDot === index ? 'current' : ''}`}
+              key={target.id}
+            >
+              {/* eslint-disable-next-line */}
+              <a
+                role="button"
+                onClick={() => onDotClicked(index)}
+              >
+                <span style={{ display: 'none' }}>
+                  {`Scroll to slide ${index}`}
+                </span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 NavDots.propTypes = {
   targets: PropTypes.arrayOf(
